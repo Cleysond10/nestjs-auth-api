99628ba4c4dd5c90665605313e9eb2872b494067
Cleysond10 <cleysonduarte99@gmail.com>
2025-03-10 20:13:06 -0300

    feat: add signup and signin endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { SignInDTO, SignUpDTO } from './dtos/auth';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() body: SignUpDTO) {
+    return this.authService.signup(body);
+  }
+
+  @Post('signin')
+  signin(@Body() body: SignInDTO) {
+    return this.authService.signin(body);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { SignInDTO, SignUpDTO } from './dtos/auth';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  signup(data: SignUpDTO) {
+    console.log(data);
+    return 'You have signed up';
+  }
+
+  signin(data: SignInDTO) {
+    console.log(data);
+    return 'You have signed in';
+  }
+}
