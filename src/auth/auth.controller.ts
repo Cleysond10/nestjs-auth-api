7eb15886ca181866e6a9264800165f8eb55b3de1
Cleysond10 <cleysonduarte99@gmail.com>
2025-03-11 00:34:15 -0300

    feat: import bcrypt and jwt to secure passwords and authenticate users sessions

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,49 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { SignInDTO, SignUpDTO } from './dtos/auth';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() body: SignUpDTO) {
+  async signup(@Body() body: SignUpDTO) {
     return this.authService.signup(body);
   }
 
   @Post('signin')
   signin(@Body() body: SignInDTO) {
     return this.authService.signin(body);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('verify-user')
+  verifyUserByAccessToken(@Request() request) {
+    return request.user;
+  }
+
+  @Get()
+  async findAll() {
+    return this.authService.findAll();
+  }
+
+  @Get('find/:email')
+  async findOne(@Param('email') email: string) {
+    return this.authService.findOne(email);
+  }
+
+  @Delete('delete/:id')
+  async delete(@Param('id') id: string) {
+    return this.authService.delete(id);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,85 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { SignInDTO, SignUpDTO } from './dtos/auth';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  signup(data: SignUpDTO) {
-    console.log(data);
-    return 'You have signed up';
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signup(data: SignUpDTO) {
+    const userAlreadyExists = await this.prismaService.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new UnauthorizedException('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: { ...data, password: hashedPassword },
+    });
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      message: 'User created successfully',
+    };
   }
 
-  signin(data: SignInDTO) {
-    console.log(data);
-    return 'You have signed in';
+  async signin(data: SignInDTO) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    const isPasswordValid = await bcrypt.compare(data.password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const accessToken = await this.jwtService.signAsync({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+
+    return {
+      accessToken,
+      message: 'User logged in successfully',
+    };
+  }
+
+  async findAll() {
+    return await this.prismaService.user.findMany();
+  }
+
+  async findOne(email: string) {
+    return await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+
+  async delete(id: string) {
+    return await this.prismaService.user.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
